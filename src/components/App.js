94294e184db3a16af08665e9cc3827066ee0cc24
lee <ljg0230@gmail.com>
2021-08-24 20:31:50 +0900

    log in 기초작업

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,26 @@
-import React from "react";
-import AppRouter from "./Router";
+import React, { useState, useEffect } from "react";
+import AppRouter from "components/Router";
+import { authService } from "fbase";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isInit, setIsInit] = useState(false);
+
+  useEffect(() => {
+    authService.onAuthStateChanged((user) => {
+      if (user) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setIsInit(true);
+    })
+  }, [])
   return (
-    <AppRouter />
+    <>
+      {isInit ? <AppRouter isLoggedIn={isLoggedIn} /> : "Initializing..."}
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
   );
 }
 
